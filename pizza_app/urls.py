6d1e5ebcc2a9cfe0b0d67fff6e3efdc85a95bd0c
MarcusCristian <cristianmarcus34@gmail.com>
2025-05-04 17:33:59 -0300

    Añade configuración de rutas para archivos estáticos en urls.py y crea render.yaml para despliegue en producción.

--- pizza_app/urls.py
+++ pizza_app/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('carta_digital.urls')),  # <-- Cambia a solo 'carta_digital.urls'
 ]
+
+# 👇 Esto agrega las rutas para archivos estáticos (CSS, JS, imágenes)
+if settings.DEBUG == False:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
