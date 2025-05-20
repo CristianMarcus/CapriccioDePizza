16a5d2a5d3c33e2820ed128c6318f7e9331774ce
MarcusCristian <cristianmarcus34@gmail.com>
2025-05-20 13:04:20 -0300

    Refactoriza el script de creación de superusuario: mejora la gestión de excepciones y actualiza la verificación de existencia del superusuario.

--- pizza_app/create_superuser.py
+++ pizza_app/create_superuser.py
@@ -1,14 +1,15 @@
-# create_superuser.py
+import os
+import django
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
+
+# Asegúrate de que la ruta al archivo de configuración sea correcta
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carta_digital.settings')  # Reemplaza 'carta_digital.settings' con la ruta a tu archivo settings.py
+django.setup()
 
 User = get_user_model()
 
-try:
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("capricho", "admin@email.com", "capricho2025")
-        print("Superusuario creado.")
-    else:
-        print("El superusuario ya existe.")
-except IntegrityError as e:
-    print(f"Error al crear superusuario: {e}")
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser('capricho', 'cristianmarcus34@gmail.com', 'capricho2025')  # Reemplaza con tus datos
+    print('Superuser creado exitosamente.')
+else:
+    print('Ya existe un superusuario.')
